a88e7691632f9e05e266dafd92d0d1675942b65a
Millan Bhandari <millan.ib@icloud.com>
2022-08-28 12:39:27 -0500

    Initial Commit

---
+++ Pick A Number/Pick_A_NumberApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pick_A_NumberApp.swift
+//  Pick A Number
+//
+//  Created by Millan Bhandari on 8/28/22.
+//
+
+import SwiftUI
+
+@main
+struct Pick_A_NumberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
