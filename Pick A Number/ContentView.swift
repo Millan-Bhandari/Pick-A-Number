34a3807a3f33d802f1b9c34c693616256def8391
Millan Bhandari <millan.ib@icloud.com>
2022-08-28 12:55:43 -0500

    found random number

--- Pick A Number/ContentView.swift
+++ Pick A Number/ContentView.swift
@@ -8,9 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var text1 = ""
+    @State private var text2 = ""
+    @State private var randomNum = 0
     var body: some View {
-        Text("Hello, world!")
+        VStack{
+        Text("Pick A Number")
+            .fontWeight(.bold)
             .padding()
+            .font(.title)
+            HStack{
+                Text("Between")
+                TextField("Number", text: $text1)
+                    .keyboardType(.numberPad)
+                    .frame(width: 65, height: 20, alignment: .center)
+                Text("And")
+                TextField("Number", text: $text2)
+                    .keyboardType(.numberPad)
+                    .frame(width: 65, height: 20, alignment: .center)
+            }
+        }
+        Button("Submit") {
+            Random()
+        }
+    }
+    func Random() {
+        if let num1 = Int(text1) {
+            if let num2 = Int(text2) {
+                randomNum = Int.random(in: num1...num2)
+            }
+        }
     }
 }
 
